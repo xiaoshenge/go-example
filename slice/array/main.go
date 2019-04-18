8db2205badc2a1319e42081ce704e0242e24cf7e
shen jian <shenjian@yixia.com>
2019-04-18 10:24:43 +0800

    通过unsafe.pointer查看变量地址

--- slice/array/main.go
+++ slice/array/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"unsafe"
 	"fmt"
 )
 var arr1 = []int{1,2}
 func main()  {
 	arr := []int{1,2,3}
+	fmt.Println(unsafe.Pointer(&arr))
 	test(arr)
+	test4(&arr)
 	fmt.Println(arr)
 	// 由于arr是slice，参数传递的是指针，对arr进行test操作后，应该是[2,3],但实际还是[1,2,3]
 	// 理解：slice里面具体里面的内容是指针，slice还是值传递, test函数里面的arr是个指拷贝，截取操作改变的是拷贝，对原来的slcie不影响
@@ -22,6 +25,7 @@ func main()  {
 	fmt.Println(arr1)
 }
 func test(arr []int)  {
+	fmt.Println(unsafe.Pointer(&arr))
 	arr = arr[1:]
 	fmt.Println(arr)
 }
@@ -32,4 +36,7 @@ func test2(arr []int)  {
 
 func test3(arr []int)  {
 	copy(arr1, arr[1:])
+}
+func test4(arr *[]int)  {
+	fmt.Println(unsafe.Pointer(arr))
 }
