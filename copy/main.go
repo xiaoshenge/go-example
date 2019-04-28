4469b612a7debbaeabe0b41bf1df89781f2bb01a
shen jian <shenjian@yixia.com>
2019-04-28 21:26:39 +0800

    copy

---
+++ copy/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+var arr = []int{1,2,3}
+var arr1 = []int{1,2,3}
+
+func main()  {
+	fmt.Println("len arr:",len(arr))
+	fmt.Println("cap arr:",cap(arr))
+	arr = arr[1:]
+
+	fmt.Println("len arr:",len(arr))
+	fmt.Println("cap arr:",cap(arr))
+
+	fmt.Println("len arr1:",len(arr1))
+	fmt.Println("cap arr1:",cap(arr1))
+
+	copy(arr1, arr1[1:])
+	fmt.Println("len arr1:",len(arr1))
+	fmt.Println("cap arr1:",cap(arr1))
+	fmt.Println(arr1)
+
+}
