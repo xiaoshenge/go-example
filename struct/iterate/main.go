ce94d277657194b2bce2278afb8fdfc6967f7fb5
shen jian <shenjian@yixia.com>
2019-05-02 00:19:57 +0800

    struct to map

---
+++ struct/iterate/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/structs"
+)
+
+
+func main() {
+	x := struct {
+		Foo string `structs:"foo" json:"foo"`
+		Bar int
+	}{"foo", 2}
+
+	// v := reflect.ValueOf(x)
+
+	// values := make([]interface{}, v.NumField())
+
+	// for i := 0; i < v.NumField(); i++ {
+	//     values[i] = v.Field(i).Interface()
+	// }
+
+	// fmt.Println(values)
+	fmt.Println(structs.Map(x))
+}
